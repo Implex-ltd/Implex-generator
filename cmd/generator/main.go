639c7445aae0b006de7ae697791ee32486931339
0xF7A4C6 <0xF7A4C6@proton.me>
2023-09-17 19:47:24 +0200

    fix task type + ua

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -19,7 +19,7 @@ var (
 		FilePath: "../../assets/input/chrome.json",
 	})
 	Build   = 227559
-	Threads = 70
+	Threads = 120
 )
 
 func worker(proxy string) {
@@ -34,7 +34,7 @@ func worker(proxy string) {
 		A11YTfe:   true,
 		Turbo:     true,
 		TurboSt:   3500,
-		TaskType:  crapsolver.TASKTYPE_NORMAL,
+		TaskType:  crapsolver.TASKTYPE_ENTERPRISE,
 	})
 
 	if err != nil {
@@ -104,54 +104,56 @@ func worker(proxy string) {
 
 	console.Log(fmt.Sprintf("<fg=64ed61>unlocked: %s</> (%d)", data.Token, s))
 
-	if err := discord.Ws.Login(); err == nil {
-		defer discord.Ws.Close()
-	}
+	go func() {
+		if err := discord.Ws.Login(); err == nil {
+			defer discord.Ws.Close()
+		}
 
-	if err != nil {
-		log.Println(err)
-		return
-	}
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-	if _, err := discord.SupressTutorial(); err != nil {
-		log.Println("tutorial", err)
-		return
-	}
+		if _, err := discord.SupressTutorial(); err != nil {
+			log.Println("tutorial", err)
+			return
+		}
 
-	avatar, _ := Assets["avatars"].Next()
-	resp, _, err := discord.PatchUser(&u.Config{
-		Date:   fmt.Sprintf("200%d-0%d-0%d", utils.RandomNumber(1, 3), utils.RandomNumber(1, 9), utils.RandomNumber(1, 9)),
-		Avatar: fmt.Sprintf("../../assets/input/avatars/%s", avatar),
-	})
-	if err != nil {
-		log.Println("avatar", err)
-		return
-	}
+		avatar, _ := Assets["avatars"].Next()
+		resp, _, err := discord.PatchUser(&u.Config{
+			Date:   fmt.Sprintf("200%d-0%d-0%d", utils.RandomNumber(1, 3), utils.RandomNumber(1, 9), utils.RandomNumber(1, 9)),
+			Avatar: fmt.Sprintf("../../assets/input/avatars/%s", avatar),
+		})
+		if err != nil {
+			log.Println("avatar", err)
+			return
+		}
 
-	log.Println("patch birth/avatar", resp.Status)
+		log.Println("patch birth/avatar", resp.Status)
 
-	bio, _ := Assets["bio"].Next()
-	resp, _, err = discord.PatchProfil(&u.Config{
-		Bio:      bio,
-		Pronouns: "he/him",
-	})
+		bio, _ := Assets["bio"].Next()
+		resp, _, err = discord.PatchProfil(&u.Config{
+			Bio:      bio,
+			Pronouns: "he/him",
+		})
 
-	if err != nil {
-		log.Println(err)
-		return
-	}
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-	log.Println("patch bio/pronouns", resp.Status)
+		log.Println("patch bio/pronouns", resp.Status)
 
-	resp, err = discord.JoinHypesquad(utils.RandomNumber(1, 3))
-	if err != nil {
-		log.Println(err)
-		return
-	}
+		resp, err = discord.JoinHypesquad(utils.RandomNumber(1, 3))
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-	log.Println("set hypesquad", resp.Status)
+		log.Println("set hypesquad", resp.Status)
 
-	utils.AppendFile("output/unlocked.txt", data.Token)
+		utils.AppendFile("output/unlocked.txt", data.Token)
+	}()
 }
 
 func main() {
